26dc401976c1d31609f384c09203d2bcafea8fb2
SpookySaikiK <149282268+SpookySaikiK@users.noreply.github.com>
2024-09-05 04:43:39 +1000

    added profile function

--- chat-app/src/app/profile/profile.component.ts
+++ chat-app/src/app/profile/profile.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  loggedInUser: any;
 
+  constructor(private router: Router) { };
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      const user = localStorage.getItem('loggedInUser');
+      if (user) {
+        this.loggedInUser = JSON.parse(user);
+
+      } else {
+        this.loggedInUser = {};
+        this.router.navigateByUrl('/login');
+      }
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('loggedInUser');
+    this.router.navigateByUrl('/login');
+  }
 }
