9f5cf0d9b2ec8cea011fff2915d4af638816bff3
SpookySaikiK <149282268+SpookySaikiK@users.noreply.github.com>
2024-09-05 00:50:06 +1000

    implemented routing

--- chat-app/src/app/app.routes.ts
+++ chat-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'profile', component: ProfileComponent}
+];
