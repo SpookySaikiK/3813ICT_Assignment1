a7345853977e3c8627d051a1728c0760a5c0ea0b
SpookySaikiK <149282268+SpookySaikiK@users.noreply.github.com>
2024-09-05 01:02:49 +1000

    added home component and routing

--- chat-app/src/app/app.routes.ts
+++ chat-app/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
     { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent},
     { path: 'profile', component: ProfileComponent}
 ];
